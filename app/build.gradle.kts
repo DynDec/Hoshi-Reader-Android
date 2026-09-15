457922e947246cd01975c246c97c8f9b15549103
DynDec <ross332cam@gmail.com>
2026-09-15 13:26:27 -0300

    1.4.5.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -113,8 +113,8 @@ android {
         applicationId = "moe.requited.hoshi"
         minSdk = 26
         targetSdk = 36
-        versionCode = 104042
-        versionName = "1.4.4.2"
+        versionCode = 104051
+        versionName = "1.4.5.1"
         releaseVersionCode?.let { versionCode = it }
         releaseVersionName?.let { versionName = it }
 
